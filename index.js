b15e0009504d1f673c07257e223dbe2e6f33904b
ayushmca4u <jkmca29@gmail.com>
2020-09-21 14:07:55 +0530

    changes

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ require("./startup/routes")(app);
 require("./startup/config")();
 require("./startup/validation")();
 
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 4000;
 const server = app.listen(port, () =>
   winston.info(`Listening on port ${port}...`)
 );
